ca0e87a5e02e31b29d8bb2663876714dd87f23bf
Biro Peter <biropeter2008@gmail.com>
2025-04-28 13:47:51 +0200

    working

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -6,27 +6,32 @@ import { useContext } from "react";
 export default function Player(){
     let [active, setActive] = useState(false);
     let currentC = useContext(PlayerContext);
-    useEffect(()=>{
-        
+    const as = document.getElementById(styles.audio_source)
+
+    useEffect(()=>{ 
+        as?.setAttribute(currentC.val == null ? "" : "src","/music/"+currentC.val?.file)
     }, [currentC]);
     return(
     <>
-       <audio controls>
-            <source  type="audio/mpeg"/>
+        <audio controls id = {styles.audio}>
+            <source id={styles.audio_source} type="audio/mpeg"/>
         </audio>
 
-        <div className = {styles.player_bar} onClick={()=>{setActive(p => !p); console.log(active)}}>
+        <div className = {styles.player_bar}>
             <img src={currentC.val == null ? "/placeholders/placeholder.png" : "/covers/" + currentC.val.cover} id= {styles.current_playing_image}></img>
-            <h1 className={styles.h1}>Dikken Titten</h1>
+            <h1 className={styles.h1}>{currentC.val == null ? "-" : currentC.val.title}</h1>
             <div id = {styles.media_buttons}>
-                <div id = {styles.previous_button}>⏪</div>
-                <div id = {styles.pause_button}>⏸️</div>
-                <div id = {styles.next_button}>⏩</div>
+                <div id = {styles.previous_button} className={styles.active_font}>⏪</div>
+                <div id = {styles.next_button} className={styles.active_font}>⏩</div>
+                <div id = {styles.pause_button} className={styles.active_font}>⏸️</div>
+
+                <div  className={styles.active_font} id = {styles.raise_page_button}
+                    onClick={()=>{setActive(p => !p)}}>⏏</div>
             </div>
         </div>
 
         <div className = {styles.player_page + " " + (active ? styles.active : "")}>
-
+            <div className={styles.active_font} onClick={()=>{setActive(p => !p)}}>▽</div>
         </div>
 
         
