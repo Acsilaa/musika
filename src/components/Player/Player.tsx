f7e00d4cb10b23eba59bb0f1233b41f012ea954f
Acsilaa-650 <gal.attila0204@gmail.com>
2025-04-28 11:15:10 +0200

    context

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from "./Player.module.css"
+import {Music, PlayerContext} from '../../App.tsx';
+import { useContext } from "react";
 
 export default function Player(){
     let [active, setActive] = useState(false);
+    let current: Music | null = useContext(PlayerContext);
+    useEffect(()=>{
+
+    }, [current]);
     return(
     <>
         <div className = {styles.player_bar} onClick={()=>{setActive(p => !p); console.log(active)}}>
-            <img src="../../../public/music.jpg" id= {styles.current_playing_image}></img>
+            <img src="/music.jpg" id= {styles.current_playing_image}></img>
             <h1 className={styles.h1}>Dikken Titten</h1>
             <div id = {styles.media_buttons}>
                 <div id = {styles.previous_button}>⏪</div>
