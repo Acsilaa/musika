3293112391104dba6ef3b19428e6f68b25c43f59
Biro Peter <biropeter2008@gmail.com>
2025-04-28 09:54:14 +0200

    what

---
+++ src/components/Player/Player.tsx
@@ -0,0 +1,17 @@
+import styles from ""
+
+export default function Player(){
+    return(
+    <>
+        {/* <div className = {styles.player_bar}>
+            <img id= {styles.current_playing_image}></img>
+            <h1>Dikken Titten</h1>
+            <div id = {styles.media_buttons}>
+                <div id = {styles.previous_button}>⏪</div>
+                <div id = {styles.pause_button}>⏸️</div>
+                <div id = {styles.next_button}>⏩</div>
+            </div>
+        </div> */}
+    </>
+)
+}
