a2199c2baf5c00aa474896127c251faef54812dc
Biro Peter <biropeter2008@gmail.com>
2025-04-28 09:55:10 +0200

    ch

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Home from './components/Home/Home.tsx';
+import Player from './components/Player/Player.tsx';
 
 function App() {
   return(
     <>
-      <Home/>
-      {/* <Player/> */}
+      {/* <Home/> */}
+      <Player/>
     </>
   )
 }
